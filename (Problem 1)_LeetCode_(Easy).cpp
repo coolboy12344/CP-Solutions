aad9cf0a517afd8180c303757c20a85f0e8f40cc
Kunal Chand <kunalchand234@gmail.com>
2020-04-20 16:40:32 +0530

    Solution to Problem 1 on LeetCode
    Solution to Problem 1 (Two Sum) on LeetCode
    
    Problem Link : https://leetcode.com/problems/two-sum/
    Solution : AC (Successfully Accepted)

--- (Problem 1)_LeetCode_(Easy).cpp
+++ (Problem 1)_LeetCode_(Easy).cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
         map<int,int> m;
         
         for(int i=0;i<nums.size();i++)

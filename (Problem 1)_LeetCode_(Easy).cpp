322a1051664b542175f9abb504532b62a6c19160
Kunal Chand <kunalchand234@gmail.com>
2020-04-20 16:51:12 +0530

    Solution to Problem 1 on LeetCode
    Solution to Problem 1 (Two Sum) on LeetCode
    
    Problem Link : https://leetcode.com/problems/two-sum/
    Solution : AC (Successfully Accepted)

--- (Problem 1)_LeetCode_(Easy).cpp
+++ (Problem 1)_LeetCode_(Easy).cpp
@@ -4,15 +4,18 @@ public:
     {
         map<int,int> m;
         
+        // Storing Vector elements in a map as "Key" and Index of the elements as "Value"
         for(int i=0;i<nums.size();i++)
         {
             m[nums[i]] = i;
         }
         
+        // Vector for storing the answer
         vector<int> ans;
         
         for(int i=0;i<nums.size();i++)
         {
+            // Check if the pair exists or not, provided both the elements have different index.
             if((m.find(target-nums[i]) != m.end()) && (i != m[target-nums[i]]))
             {
                 ans.push_back(i);
